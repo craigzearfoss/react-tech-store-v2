7da7da399cb7defd95dc2ae5b0e60c472bdebb00
Craig Zearfoss <craigzearfoss@yahoo.com>
2020-05-24 20:31:30 -0500

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        I'm baby marfa plaid gluten-free bushwick, unicorn echo park locavore
+        lumbersexual skateboard. Marfa portland tumeric, yr chambray subway tile
+        tilde. Yr plaid tilde, photo booth retro put a bird on it 90's
+        williamsburg church-key cray freegan swag humblebrag irony slow-carb.
+        PBR&B post-ironic beard affogato, viral put a bird on it mumblecore
+        franzen kickstarter.
+      </p>
+      <p>
+        Iceland dreamcatcher tilde DIY taxidermy thundercats waistcoat brunch
+        fingerstache kinfolk lomo williamsburg. Vice hell of irony skateboard
+        chartreuse affogato salvia cred. Authentic kombucha pug meditation
+        humblebrag, cold-pressed scenester tumblr woke four loko XOXO. Stumptown
+        tumblr lumbersexual, asymmetrical la croix shabby chic skateboard banh
+        mi meditation crucifix hell of everyday carry biodiesel literally.
+        Affogato pitchfork gastropub intelligentsia, migas stumptown twee banh
+        mi occupy taiyaki.
+      </p>
+    </section>
+  );
+}
